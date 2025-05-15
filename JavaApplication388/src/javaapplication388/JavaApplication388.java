36852f3dcc0d08ab44ccc084f659d117738d592b
JoaoCOD2 <joao.github23@gmail.com>
2025-05-15 15:14:20 -0300

    Atualização!

--- JavaApplication388/src/javaapplication388/JavaApplication388.java
+++ JavaApplication388/src/javaapplication388/JavaApplication388.java
@@ -1,9 +1,23 @@
 
 package javaapplication388;
+
+import java.util.Scanner;
+
 public class JavaApplication388 {
 
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Scanner ler = new Scanner (System.in);
+        int n1, n2, conta;
+        
+        System.out.println("Escolha um numero");
+        n1 = ler.nextInt();
+        System.out.println("Escolha outro numero");
+        n2 = ler.nextInt();
+        conta = n1 + n2;
+        
+        System.out.println("O resultado é " + conta);
+        
+        
     }
     
 }
